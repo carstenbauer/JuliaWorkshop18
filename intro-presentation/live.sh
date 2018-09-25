7046dda1af8327357fd9d6aec0b54c616cb2dab7
Carsten Bauer <crstnbr@gmail.com>
2018-09-25 16:56:54 +0200

    intro-presentation

---
+++ intro-presentation/live.sh
@@ -0,0 +1,1 @@
+reveal-md -w presentation.md --scripts script.js
---
+++ intro-presentation/script.js
@@ -0,0 +1,12 @@
+// Either Reveal.initialize({ ... or
+Reveal.configure({
+    keyboard: {
+        38: 'prev',
+        40: 'next'
+    },
+	// The "normal" size of the presentation, aspect ratio will be preserved
+    // when the presentation is scaled to fit different resolutions. Can be
+    // specified using percentage units.
+    width: 1100,
+    height: 800
+})
