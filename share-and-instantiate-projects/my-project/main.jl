2776c9641ade7ad87598d11352b143d26d0d8651
Carsten Bauer <crstnbr@gmail.com>
2018-09-25 15:19:49 +0200

    update

---
+++ share-and-instantiate-projects/my-project/main.jl
@@ -0,0 +1,7 @@
+using Useless # let's say our project depends on this useless package: https://github.com/crstnbr/Useless.jl
+
+include("somedefinitions.jl")
+
+println(life_universe_everything("What is the meaning of life?"))
+
+# Done.
---
+++ share-and-instantiate-projects/my-project/somedefinitions.jl
@@ -0,0 +1,2 @@
+# Defining a function
+life_universe_everything(args...) = 42
