ebc934d09e80b7f5f426f08686d3e5121e7ac2f9
Carsten Bauer <crstnbr@gmail.com>
2018-09-26 12:58:19 +0200

    profiling

---
+++ 2 Tips and tricks/profiling/myprogram.jl
@@ -0,0 +1,24 @@
+function myfunction(n)
+    for i = 1:n
+        A = randn(100,100,20)
+        m = maximum(A)
+        Am = mapslices(sum, A; dims=2)
+        B = A[:,:,5]
+        Bsort = mapslices(sort, B; dims=1)
+        b = rand(100)
+        C = B.*b
+    end
+end
+
+
+# Do the following in the Juno IDE
+using Profile
+Profile.clear()
+myfunction(1) # trigger compilation
+@profile myfunction(10)
+# up to here, everything is the same as for using ProfileView.jl
+
+# Now comes the cool part. Visualize your profile results:
+Juno.profiler()
+# or simply
+@profiler myfunction(10)
